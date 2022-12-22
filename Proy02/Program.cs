14d6a000726f09b2ddc46335627f6160003ae765
Yosmary17 <yosmaryasis17@gmail.com>
2022-12-22 08:45:29 -0500

    Actualizacion Figura

--- Proy02/Program.cs
+++ Proy02/Program.cs
@@ -5,7 +5,15 @@
         private static void Main(string[] args)
         {
 
-            //Figura cuadrado = new Figura();
+            Cuadrado  cuad = new Cuadrado();
+            cuad.Lado = 10;
+            cuad.Nombre="Mi cuadrado";
+            cuad.LineColor= "#F00000";
+            Console.WriteLine($"Nombre: {cuad.Nombre} ");
+            Console.WriteLine($"ID: {cuad.Id} ");
+            Console.WriteLine($"Lado: {cuad.Lado} ");
+            Console.WriteLine($"Area: {cuad.getSize()} ");
+            Console.WriteLine($"Perimetro: {cuad.getPerimetro()}");
         }
     }
 }
